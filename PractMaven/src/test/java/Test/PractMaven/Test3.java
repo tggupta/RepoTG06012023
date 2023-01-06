4b9824352b1716b0cd627f93fdc17aa2fa969d4a
Tarun Gupta <tarungupta02@IN-PG02A8L9.Nagarro.local>
2023-01-06 13:38:29 +0530

    TG2Branch change in Test3
    Signed-off-by: Tarun Gupta <tarungupta02@IN-PG02A8L9.Nagarro.local>

--- PractMaven/src/test/java/Test/PractMaven/Test3.java
+++ PractMaven/src/test/java/Test/PractMaven/Test3.java
@@ -3,7 +3,7 @@ package Test.PractMaven;
 public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("This is Test3");
+		System.out.println("This is Test3 - TG2 branch");
 	}
 	
 }
