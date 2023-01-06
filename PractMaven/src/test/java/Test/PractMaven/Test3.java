b7cfc85cb05a89f779e04bede7d738bed9821f25
Tarun Gupta <tarungupta02@IN-PG02A8L9.Nagarro.local>
2023-01-06 23:04:16 +0530

    Feature TG - Added change 1
    Signed-off-by: Tarun Gupta <tarungupta02@IN-PG02A8L9.Nagarro.local>

--- PractMaven/src/test/java/Test/PractMaven/Test3.java
+++ PractMaven/src/test/java/Test/PractMaven/Test3.java
@@ -3,7 +3,7 @@ package Test.PractMaven;
 public class Test3 {
 
 	public static void main(String[] args) {
-
+		//change 1
 		System.out.println("This is Test3 - TG2 branch");
 		System.out.println("Modifications in Test3");
 		System.out.println("Main branch change");
